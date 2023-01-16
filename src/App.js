ddb2fc34610f23968a6adc45ba6f98b1a36f4532
Yogesh Agrawal <yogeshagrawal50@gmail.com>
2023-01-16 23:12:13 +0530

    added sidebar and headers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
 import { ColorModeContext, useMode } from './theme';
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
 import Topbar from './pages/global/Topbar'
+import Sidebar from "./pages/global/Sidebar";
+import Dashboard from "./pages/dashboard";
+// import Team from "./pages/team";
+// import Invoices from "./pages/invoices";
+// import Contacts from "./pages/contacts";
+// import Bar from "./pages/bar";
+// import Form from "./pages/form";
+// import Line from "./pages/line";
+// import Pie from "./pages/pie";
+// import FAQ from "./pages/faq";
+// import Geography from "./pages/geography";
+// import Calendar from "./pages/calendar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -11,9 +24,22 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <Sidebar/>
           <main className="content">
             <Topbar />
-
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              {/* <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/geography" element={<Geography />} /> */}
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
+import Header from "../../components/Headers";
+
+import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
+import { tokens } from "../../theme";
+import { mockTransactions } from "../../data/mockData";
+import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
+import EmailIcon from "@mui/icons-material/Email";
+import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import TrafficIcon from "@mui/icons-material/Traffic";
+// import LineChart from "../../components/LineChart";
+// import GeographyChart from "../../components/GeographyChart";
+// import BarChart from "../../components/BarChart";
+// import StatBox from "../../components/StatBox";
+// import ProgressCircle from "../../components/ProgressCircle";
 
 const index = () => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
   return (
-    <div>index</div>
-  )
-}
+    <Box m="20px">
+      {/* HEADER */}
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
 
-export default index
+        <Box>
+          <Button
+            sx={{
+              backgroundColor: colors.blueAccent[700],
+              color: colors.grey[100],
+              fontSize: "14px",
+              fontWeight: "bold",
+              padding: "10px 20px",
+            }}
+          >
+            <DownloadOutlinedIcon sx={{ mr: "10px" }} />
+            Download Reports
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default index;
