422e02fce5a998ad823390617645269b642cc60e
Yogesh Agrawal <yogeshagrawal50@gmail.com>
2023-01-17 12:49:25 +0530

    profile form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import "./App.css";
-import { ColorModeContext, useMode } from './theme';
+import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { Routes, Route } from "react-router-dom";
-import Topbar from './pages/global/Topbar'
+import Topbar from "./pages/global/Topbar";
 import Sidebar from "./pages/global/Sidebar";
 import Dashboard from "./pages/dashboard";
 import Team from "./pages/team";
-// import Invoices from "./pages/invoices";
-// import Contacts from "./pages/contacts";
+import Invoices from "./pages/invoices";
+import Contacts from "./pages/contacts";
 // import Bar from "./pages/bar";
-// import Form from "./pages/form";
+import Form from "./pages/form";
 // import Line from "./pages/line";
 // import Pie from "./pages/pie";
 // import FAQ from "./pages/faq";
@@ -30,10 +30,10 @@ function App() {
             <Routes>
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
-              {/* <Route path="/contacts" element={<Contacts />} />
+              <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
-              <Route path="/bar" element={<Bar />} />
+              {/*<Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
               <Route path="/faq" element={<FAQ />} />
